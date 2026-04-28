fcfea96371412991b9ce7ccff97ed3ba005c5790
nickisingg <nsingl14@students.kennesaw.edu>
2026-04-28 02:44:52 -0400

    FEAT: Add database helper functions for inserting and retrieving pantry items.

--- database_manager.py
+++ database_manager.py
@@ -18,5 +18,24 @@ def init_db():
     conn.close()
     print("Database initialized and table created.")
 
+def add_item(name, qty):
+    conn = sqlite3.connect('pantry.db')
+    cursor = conn.cursor()
+    # This command inserts new row of data into the table
+    cursor.execute("INSERT INTO inventory (item_name, quantity) VALUES (?, ?)", (name, qty))
+    conn.commit()
+    conn.close()
+    print(f"Added {qty} of {name} to the pantry.")
+
+def get_inventory():
+    conn = sqlite3.connect('pantry.db')
+    cursor = conn.cursor()
+    # This selects everything from the table
+    cursor.execute("SELECT item_name FROM inventory")
+    # Use list comprehension to get the names
+    items = [row[0] for row in cursor.fetchall()]
+    conn.close()
+    return items
+
 if __name__ == "__main__":
     init_db()
